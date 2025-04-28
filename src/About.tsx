9c34ccf2122c23e5c0fb008f023c0e239dec0ac2
emerrafter1 <emerlouiserafter@gmail.com>
2025-04-28 16:20:30 +0100

    add about me

--- src/About.tsx
+++ src/About.tsx
@@ -1,5 +1,195 @@
 const About = () => {
-  return <section className="about-me"></section>;
+  return (
+    <section className="about-me">
+      <div>
+        <h2>Personal Statement</h2>
+        <p>
+          Senior QA Engineer turned Software Developer with 5 years of
+          experience in manual and automated testing. Recently completed a
+          full-time Software Development Bootcamp at Northcoders, where I built
+          full-stack JavaScript applications using React, Node.js, and
+          databases, with a strong focus on test-driven development. Skilled in
+          Cypress, writing test cases, and building automation scripts, plus
+          hands-on coding experience from the bootcamp. Now looking to apply my
+          QA background and new development skills to build and ship
+          high-quality software solutions.
+        </p>
+      </div>
+      <div>
+        <h2>Technical Skills</h2>
+        <ul>
+          <li>
+            <strong>Languages: </strong>JavaScript, TypeScript, using Node.js
+            and browsers as environment
+          </li>
+          <li>
+            <strong>Testing: </strong>TDD (Test Driven Development), Jest,
+            Supertest, Cypress, Postman, TestRail
+          </li>
+          <li>
+            <strong>Monitoring and debugging</strong>Google Cloud Logging and
+            Logrocket
+          </li>
+          <li>
+            <strong>Backend: </strong>MySql, PSQL, seeding
+          </li>
+          <li>
+            <strong>Frontend</strong>HTML, CSS, React, Lighthouse and other
+            accessibility tools
+          </li>
+          <li>
+            <strong>Hosting</strong>Netlify, Render
+          </li>
+          <li>
+            <strong>Project Management & Collaboration</strong>JIRA, Agile,
+            SCRUM, Kanban, Confluence, Trello
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2>Professional Development</h2>
+        <h3>Northcoders Software Development Bootcamp in JavaScript</h3>
+        <p></p>Completed a 13-week full-stack JavaScript bootcamp at
+        Northcoders, gaining hands-on experience in software development. Built
+        and deployed full-stack applications using React, Node.js, and
+        databases, with a focus on test-driven development and agile
+        methodologies. Worked in a team using Git, pair programming, and
+        problem-solving to deliver a real-world project.
+      </div>
+      <div>
+        <h3>Experiences</h3>
+        <h4>
+          Senior Quality Assurance Engineer | GetGround, London | Jan, 2023 -
+          Oct, 2024
+        </h4>
+        <ul>
+          <li>
+            Developed and implemented QA strategies at the project level for the
+            team, identifying and executing necessary QA activities such as
+            automation, end-to-end, manual and exploratory testing
+          </li>
+          <li>
+            Reviewed end-to-end test results on a daily basis, ensuring timely
+            identification and resolution of issues from automated test failures{" "}
+          </li>
+          <li>
+            Provided production support on a daily basis, acted as primary point
+            of contact between the customer support and engineering teams,
+            efficiently triaging, documenting, prioritising issues
+          </li>
+          <li>
+            Analysed server logs, user sessions and the database to diagnose the
+            root cause of bugs/defects
+          </li>
+          <li>
+            Experienced working with third party integrations including Stripe,
+            Modulr, CurrencyCloud, Onfido, WealthKernel and HubSpot
+          </li>
+          <li>
+            Conducted API testing using Postman and Swagger to validate response
+            data
+          </li>
+          <li>
+            Collaborated with Product and UX Design teams to ensure
+            comprehensive coverage of all user journeys and to define acceptance
+            criteria during early-stage feature development
+          </li>
+          <li>
+            Managed junior colleagues by guiding test plan reviews, offering
+            constructive feedback to enhance the quality and coverage of testing
+            and overseeing the triaging, prioritisation and resolution of issues
+          </li>
+          <li>
+            Led squad ceremonies, including daily stand-ups, release planning,
+            refinement sessions
+          </li>
+          <li>
+            Consistently demonstrated excellence by being the only individual in
+            a company of 85 to achieve a maximum rating in the year-end review
+            for 2023
+          </li>
+        </ul>
+        <h4>
+          Technology Consultant | First Derivatives, London | Sep, 2019 - Jan
+          2023
+        </h4>
+        <p>
+          During my time at First Derivatives I worked on several projects and
+          held the following roles:
+        </p>
+        <h4>
+          QA Analyst | London Stock Exchange Group, London | Aug, 2021 - Jan
+          2023
+        </h4>
+        <ul>
+          <li>
+            Led the timely delivery of quality product releases based off
+            stakeholders’ specifications
+          </li>
+          <li>
+            Created and executed test scenarios and created automated testing
+            suites{" "}
+          </li>
+          <li>
+            Collaborated development teams in order to identify the root-cause
+            of bugs and defects based on troubleshooting and analysis of testing
+            evidence
+          </li>
+          <li>
+            Managed junior QAs through onboarding, training and
+            knowledge-sharing sessions
+          </li>
+        </ul>
+        <h4>Support Analyst | Nephila, London | Mar, 2021 - Aug 2021</h4>
+        <ul>
+          <li>
+            Supported users in the form of answering queries, resolving
+            problems, assisting with bug fixes
+          </li>
+          <li>
+            Identified areas of improvement within the application based on
+            analysis off frequently seen issues
+          </li>
+          <li>
+            Designed and created an FAQ site for users to quickly resolve their
+            issues independently
+          </li>
+        </ul>
+        <h4>
+          QA Analyst | Investec Wealth & Investment, London | Mar, 2020 - Mar
+          2021
+        </h4>
+        <ul>
+          <li>
+            Performed User Acceptance Testing based on client specifications
+          </li>
+          <li>
+            Prepared and delivered thorough QA testing reports and presented
+            findings to stakeholders
+          </li>
+          <li>
+            Created user guides, release notes, and product documentation for
+            business users on Confluence
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3>Education</h3>
+        <ul>
+          <li>
+            M.A.I Biomedical Engineering | Trinity College Dublin | 2018 - 2019
+          </li>
+          <li>
+            B.A.I Biomedical Engineering | Trinity College Dublin | 2014 - 2018
+          </li>
+          <li>B.A Mathematics | Trinity College Dublin | 2014 - 2018</li>
+          <li>
+            Professional Diploma, UX Design | UX Design Institute | 2021 - 2022
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default About;
