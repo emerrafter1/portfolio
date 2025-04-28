4b72c8a18731326e0734caef8a6df2c5348deb3f
emerrafter1 <emerlouiserafter@gmail.com>
2025-04-28 15:52:02 +0100

    project set up

---
+++ src/About.tsx
@@ -0,0 +1,5 @@
+const About = () => {
+  return <section className="about-me"></section>;
+};
+
+export default About;
---
+++ src/Nav.tsx
@@ -0,0 +1,14 @@
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Resume</li>
+        <li>Project</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
