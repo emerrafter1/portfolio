4a2aa079358cd4f47f337c578611162b95c07999
emerrafter1 <emerlouiserafter@gmail.com>
2025-04-30 11:33:49 +0100

    ICONCL

--- src/About.tsx
+++ src/About.tsx
@@ -1,6 +1,5 @@
 import { experienceData } from "./data/experience_data";
-import { SiReact, SiTailwindcss, SiTypescript, SiNodedotjs , SiJest, SiCypress, SiPostman, SiTestrail, SiGooglecloud, SiMysql} from "react-icons/si";
-
+import TechGrid from "./TechGrid";
 
 const About = () => {
   return (
@@ -9,40 +8,11 @@ const About = () => {
         <p>{experienceData.bio}</p>
       </div>
 
-      <div className="mb-24">
-      
-      <SiTailwindcss className="w-12 h-12 text-purple-300" />
-      <SiTypescript className="w-12 h-12 text-purple-300" />
-      <SiNodedotjs className="w-12 h-12 text-purple-300" />
+      <div className="mb-24 justify-center">
+        <h2 className="uppercase text-center mb-8 border-4 p-4 text-xl">Tools & Technologies</h2>
 
-      <SiJest className="w-12 h-12 text-purple-300" />
-      <SiCypress className="w-12 h-12 text-purple-300" />
-      <SiPostman className="w-12 h-12 text-purple-300" />
-      <SiTestrail className="w-12 h-12 text-purple-300" />
-
-      <SiGooglecloud className="w-12 h-12 text-purple-300" />
-
-      <SiReact className="w-12 h-12 text-purple-300" />
-   
-        <ul>
-          {Object.entries(experienceData.technicalSkills).map(
-            ([key, value]) => (
-              <li key={key}>
-                <strong>
-                  {key.charAt(0).toUpperCase() +
-                    key.slice(1).replace(/([A-Z])/g, " $1")}
-                  :{" "}
-                </strong>
-                {value}
-              </li>
-            )
-          )}
-        </ul>
+        <TechGrid />
       </div>
-
-
-
-
     </section>
   );
 };
--- src/Header.tsx
+++ src/Header.tsx
@@ -2,6 +2,9 @@ type HeaderProps = {
   activeSection: string;
 };
 
+import { SiLinkedin, SiGithub } from "react-icons/si";
+
+
 const Header: React.FC<HeaderProps> = ({ activeSection }) => {
   const navItems = [
     { id: "about", label: "About" },
@@ -11,29 +14,37 @@ const Header: React.FC<HeaderProps> = ({ activeSection }) => {
   ];
 
   return (
-    <header>
+<header className="flex flex-col h-full">
+  <h1 className="text-5xl">Emer Louise Rafter</h1>
+  <h2 className="text-xl my-12">Full Stack Engineer</h2>
 
-      <h1 className="text-5xl">Emer Louise Rafter</h1>
-      <h2 className="text-xl my-12">Full Stack Engineer</h2>
-  
+  <nav className="my-12">
+    <ul className="flex flex-col gap-2">
+      {navItems.map(({ id, label }) => (
+        <li key={id} className="uppercase">
+          <a
+            href={`#${id}`}
+            className={`transition-colors duration-300 ${
+              activeSection === id ? "text-violet-400 font-bold" : "text-violet-200"
+            }`}
+          >
+            {label}
+          </a>
+        </li>
+      ))}
+    </ul>
+  </nav>
 
-    <nav className="my-12">
-      <ul className="flex flex-col gap-2">
-        {navItems.map(({ id, label }) => (
-          <li key={id} className="uppercase">
-            <a
-              href={`#${id}`}
-              className={`transition-colors duration-300 ${
-                activeSection === id ? "text-violet-400 font-bold" : "text-violet-200"
-              }`}
-            >
-              {label}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
-    </header>
+
+  <div className="mt-48     flex flex-wrap gap-4">
+    <a href="https://www.linkedin.com/in/emerrafter/">
+   <SiLinkedin className="w-8 h-8 text-purple-300 text-opacity-60 hover:text-opacity-100 hover:cursor-pointer"/></a>
+   <a href="https://github.com/emerrafter1">
+   <SiGithub className="w-8 h-8 text-purple-300 text-opacity-60 hover:text-opacity-100 hover:cursor-pointer"/></a>
+
+  </div>
+</header>
+
   );
 };
 
